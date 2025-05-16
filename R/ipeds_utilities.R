4d64bbc1350ce23910587d5b91686e43872255d0
David Eubanks <stanislavzza@gmail.com>
2025-05-16 10:21:53 -0400

    Update ipeds_utilities.R
    Eliminated the odbc qualifier so duckdb will work

--- R/ipeds_utilities.R
+++ R/ipeds_utilities.R
@@ -5,7 +5,7 @@
 #' @export
 my_dbListTables <- function(idbc, search_string){
 
-  tables <- odbc::dbListTables(idbc, table_type = "TABLE")
+  tables <- dbListTables(idbc, table_type = "TABLE")
   tables <- tables[str_detect(tables, search_string)]
 
   return(tables)
