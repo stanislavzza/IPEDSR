69ec2456694f57ecf02a0fd8b5eef0d50983a603
Stanislav Zza <stanislavzza@gmail.com>
2025-05-19 12:06:19 -0400

    Changed test.R to a short test with duckdb

---
+++ test.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(DBI)
+library(duckdb)
+library(IPEDSR)
+
+# ipeds database connector -- change the path to your db
+idbc <- duckdb::dbConnect(duckdb(), dbdir = "data/ipeds.duckdb", read_only = TRUE)
+
+# get the list of tables
+ipeds_tables <- duckdb::dbListTables(idbc, table_type = "TABLE")
+
+# View a few rows of the header
+tbl(idbc, "HD2023") |>
+  select(UNITID, INSTNM, CITY, STABBR) |>
+  head(10) |>
+  collect()  # Collect pulls the data into R as a regular dataframe
+
